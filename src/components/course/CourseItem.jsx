1f0ad4e9a0960532e9151769243588784a89c212
youngjinjung <jyj1641@gmail.com>
2024-10-18 14:55:46 +0900

    JSX 에서 자바스크립트 사용

--- src/components/course/CourseItem.jsx
+++ src/components/course/CourseItem.jsx
@@ -1,11 +1,26 @@
-export default function CourseItem(){
+
+export default function CourseItem(props){
+
+  const isEmpty = false;
+  if(isEmpty)
+  {
+    return (<p>강의가 없습니다.</p>)
+  }
+
+  const course = {
+    title: '입문자를 위한, HTML&CSS 웹 개발 입문',
+    descript: '웹 개발에 필요한 기본 지식을 배웁니다.',
+    image: './img/htmlcss.png',
+    alt: '웹 개발에 필요한 기본 지식을 배웁니다.',
+
+  }
 
   return (
       <article className="course">
-        <img className="course__img" src="./img/htmlcss.png" alt="" />
+        <img className="course__img" src={course.image} alt={course.alt} />
         <div className="course__body">
-          <div className="course__title">입문자를 위한, HTML&CSS 웹 개발 입문</div>
-          <div className="course__description">웹 개발에 필요한 기본 지식을 배웁니다. </div>
+          <div className="course__title">{course.title}</div>
+          <div className="course__description">{course.descript}</div>
         </div>
       </article>
   );
--- src/components/course/CourseListCard.jsx
+++ src/components/course/CourseListCard.jsx
@@ -3,13 +3,13 @@ import CourseItem from './CourseItem';
 
 export default function CourseListCard() {
   return (
-    <div className="card">
+    <div className="card" style={{ backgroundColor: 'black', color: 'white'}}>
 			<div className="card__header">강의 목록</div>
 			<div className="card__body">
-				<div className="courses">
-					<CourseItem />
-          <CourseItem />
-          <CourseItem />
+				<div className="courses" style={{backgroundColor: '#9e9e9e'}}>
+					<CourseItem title="강의가 없다잉!"/>
+          <CourseItem title="강의가 없다잉!"/>
+          <CourseItem title="강의가 없다잉!"/>
 				</div>
 			</div>
 		</div>
