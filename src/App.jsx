6b0cf7e656e959f660d92cd5b81a6884642ee678
youngjinjung <jyj1641@gmail.com>
2024-10-19 19:52:35 +0900

    조건부 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,15 @@ function App() {
       title:'입문자를 위한, HTML&CSS 웹 개발 입문',
 		  descript:'웹 개발에 필요한 기본 지식을 배웁니다.',
 		  thumbnail: '/imgs/htmlcss.jpg',
-      isFavorite: true,
+      isFavorite: true, 
+      link: 'https://inf.run/JxyyT'
     },
     {
       title:'입문자를 위한, ES6+ 최신 자바스크립트 입문',
       descript:'쉽고! 알찬! 내용을 준비했습니다.',
       thumbnail: '/imgs/js.jpeg',
       isFavorite: false,
+      link: 'https://inf.run/Kpnd'
 
     },
     {
@@ -21,7 +23,7 @@ function App() {
       descript:'포트폴리오 사이트를 만들고 배표해 보세요.',
       thumbnail: '/imgs/portfolio.jpeg',
       isFavorite: true,
-      
+      link: 'https://inf.run/YkAN'
     }
   ]
   return (
