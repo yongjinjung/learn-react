63649e4e5418d97ba3d52fb9097fffd7c0194974
eJwtzMFuAiEQgOF3mTMHhVUpt7aXpsa0aby0FzLAaOniQoZdlRjfvYnZ6598/w3Qe6p1n3sawMBByy44vdBy4b2TS+XXGhXiqlsFXJLr9MZ5lAgCCudzDMQzvc0nO86r42+2X1n+XXcjlV5t2/M5PX2rT719f9ns317bj/pY466BgAexYysEBhwhE4OA6vMjYDjFwZTJpehtT00wYTCZj4KpZDFVYkMnjElcMveHlC8ggK4lMlWLI5hhSul+/wevqlB9 <jyj1641@gmail.com>
2024-10-24 23:02:46 +0900

    useEffect Hook

---
+++ src/AppEffect.jsx
@@ -0,0 +1,59 @@
+import React, { Fragment, useEffect, useState } from 'react';
+
+function Courses(props) {
+
+  const [list, setList] = useState([]);
+  const [filter, setFilter] = useState('all');
+
+  useEffect(()=>{
+    //const jsonData = filter === 'all'? 'courses_all.json':'courses_favorite.json';
+    fetch(`data/courses_${filter}.json`)
+    .then((res) => res.json())
+    .then( data => {
+      console.log('👍😁데이터 조회 성공');
+      setList(data)
+    })
+
+    return ()  => {
+      console.log('😒 연결 해제 ~!');
+
+    }
+  },[filter])  
+
+  return (
+    <Fragment>
+      <label htmlFor="all">전체</label>
+      <input id="all" type="radio" value="all" checked={filter === 'all'} onChange={(e) => setFilter(e.target.value)}/>
+      <label htmlFor="favorite">좋아요</label>
+      <input id="favorite" type="radio" value="favorite" checked={filter === 'favorite'} onChange={(e) => setFilter(e.target.value)}/>
+      <ul>
+        {list.map( item => 
+          (<li key={item.id}>{item.title}</li>)
+        )}
+      </ul>
+    </Fragment>
+  )
+
+}
+
+function AppEffect(props) {
+
+   // 1] DOM 조작하기
+  // useEffect(() => {
+  //   const $h2 = document.querySelector('#title');
+  //   $h2.textContent = 'Data Fetching';
+  // }, []);
+  const [show, setShow] = useState(true);
+  
+
+  return (
+    <>
+      <h2 id="title">데이터 가져오기기</h2>
+      <button onClick={ (e) =>{ setShow(!show)}}>toggle</button>
+      <hr />
+      {show && <Courses />}
+    </>
+  );
+}
+
+export default AppEffect;
